6007c72cee187f9e37e642bd129f6bbef271c8ba
Chase Dorsey <git@chase-dorsey.com>
2024-04-06 19:14:26 -0500

    37 current  2024-04-06 19:14:24  23.11.20240311.ddcd759   Unknown                          *

--- configuration.nix
+++ configuration.nix
@@ -10,28 +10,30 @@ let
   nixosSwitch = pkgs.writeScriptBin "nixos-switch" ''
     set -e
 
+    GIT=${pkgs.git}
+
     # I'm lazy, so if we're not in the right directory, just bail
     if [[ $PWD != ~/.dotfiles ]]; then
       exit 0
     fi
 
     # Update the user config if needed
-    if ! git diff --quiet --ignore-all-space -- home.nix; then
+    if ! $GIT diff --quiet --ignore-all-space -- home.nix; then
       home-manager switch --flake .
-      git add home.nix
-      git commit -m "$(home-manager generations | head -n1)"
+      $GIT add home.nix
+      $GIT commit -m "$(home-manager generations | head -n1)"
     fi
 
     # Update the system config if needed
-    if ! git diff --quiet --ignore-all-space -- *.nix; then
+    if ! $GIT diff --quiet --ignore-all-space -- *.nix; then
       sudo nixos-rebuild switch --flake .
-      git add *.nix
-      git commit -m "$(nixos-rebuild list-generations 2>/dev/null | grep current)"
+      $GIT add *.nix
+      $GIT commit -m "$(nixos-rebuild list-generations 2>/dev/null | grep current)"
     fi
 
     # If there are remaining changed files, amend them to the last commit
-    if ! git diff --quiet --ignore-all-space; then
-      git commit --verbose --all --no-edit --amend
+    if ! $GIT diff --quiet --ignore-all-space; then
+      $GIT commit --verbose --all --no-edit --amend
     fi
   '';
 in
