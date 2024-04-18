5951786daa9cfc032adde27a3fa03a9707c628bb
Chase Dorsey <git@chase-dorsey.com>
2024-04-17 22:21:51 -0500

    55 current  2024-04-17 22:20:11  24.05.20240416.5672bc9   Unknown                          *

--- system/scripts/nixos-switch.nix
+++ system/scripts/nixos-switch.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 let
   git = "${pkgs.git}/bin/git";
-  nixfmt = "${pkgs.nixfmt}/bin/nixfmt";
-in pkgs.writeShellScriptBin "nixos-switch" ''
+  nixfmt = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+  nh = "${pkgs.nh}/bin/nh";
+in
+pkgs.writeShellScriptBin "nixos-switch" ''
   set -e
 
   # I'm lazy, so if we're not in the right directory, just bail
@@ -10,18 +12,18 @@ in pkgs.writeShellScriptBin "nixos-switch" ''
     exit 0
   fi
 
-  ${nixfmt} $(${git} ls-files -m -x *.nix)
+  ${nixfmt} $(${git} ls-files -m | grep '\.nix$')
 
   # Update the system config if needed
-  if ! ${git} diff --quiet --ignore-all-space -- system/**/*.nix; then
-    sudo nixos-rebuild switch --flake .
+  if ! ${git} diff --quiet --ignore-all-space -- flake.nix system/**/*.nix; then
+    ${nh} os switch
     ${git} add system/**/*.nix
     ${git} commit -m "$(nixos-rebuild list-generations 2>/dev/null | grep current)"
   fi
 
   # Update the user config if needed
   if ! ${git} diff --quiet --ignore-all-space -- user/**/*.nix; then
-    home-manager switch --flake .
+    ${nh} home switch
     ${git} add user/**/*.nix
     ${git} commit -m "$(home-manager generations | head -n1)"
   fi
