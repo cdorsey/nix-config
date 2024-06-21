94346aaeef3c38d023771402ff4460502819489f
Chase Dorsey <git@chase-dorsey.com>
2024-06-20 22:28:02 -0400

    switch to cinnamon

--- hosts/hermes/configuration.nix
+++ hosts/hermes/configuration.nix
@@ -131,7 +131,8 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
