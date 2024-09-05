780c41412fc053d3bbe471e530556c22a162acd2
Chase Dorsey <git@chase-dorsey.com>
2024-09-04 20:52:06 -0400

    Configure wezterm

--- homeManagerModules/wezterm.nix
+++ homeManagerModules/wezterm.nix
@@ -1,10 +1,21 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
   programs.wezterm = {
     enable = true;
-    package = pkgs.wezterm;
 
     enableZshIntegration = config.programs.zsh.enable;
     enableBashIntegration = config.programs.bash.enable;
+
+    extraConfig = # lua
+      ''
+        return {
+          font = wezterm.font {
+            family = "FiraCode Nerd Font Mono",
+            harfbuzz_features = { "ss07" }
+          },
+          font_size = 12,
+          window_background_opacity = 0.9,
+        }
+      '';
   };
 }
